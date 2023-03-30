32ce41bec5e99a85f14505bf16d74387e4c37484
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-30 12:44:51 +0530

    removed services tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,16 +16,6 @@ export default function Navbar(props) {
               <li className="nav-item">
                 <a className="nav-link" href="/">Link</a>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  {props.services}
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">{props.live}</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="/">ES7 Lint</a></li>   
-                </ul>
-              </li>
             </ul>
           </div>
         </div>
